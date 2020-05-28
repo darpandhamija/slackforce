d5a2110b08e91036ac2a3af05421edbb47d09b3a
Darpan <ddhamija@salesforce.com>
2020-05-28 14:37:09 -0700

    test

--- modules/opportunity.js
+++ modules/opportunity.js
@@ -55,7 +55,7 @@ exports.execute = (req, res) => {
         })
         .catch(error => {
             if (error.code == 401) {
-                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId+'   '+error);
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId+'   '+JSON.stringify(error));
             } else {
                 console.log(error);
                 res.send("An error as occurred");
