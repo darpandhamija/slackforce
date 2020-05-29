c02e7a00a4ceb50cff662ddfcff82aa661a14135
Darpan <ddhamija@salesforce.com>
2020-05-28 22:34:49 -0700

    new actions

--- modules/actions.js
+++ modules/actions.js
@@ -4,14 +4,14 @@ exports.handle = (req, res) => {
     console.log(req.body);
     let attachments = [];
     let fields = [];
-   	fields.push({title: "```/whoami", value: "User Info", short: false});
-    fields.push({title: "```/cadences", value: "List of Sales Cadences", short: false});
-    fields.push({title: "```/targets", value: "List of recent Targets (Lead, Contact, Person Account)", short: false});
-    fields.push({title: "```/wq", value: "Work Queue", short: false});
-    fields.push({title: "```/addToCadence", value: "Assign Target to Sales Cadence", short: false});
-    fields.push({title: "```/removeFromCadence", value: "Remove Target from Sales Cadence", short: false});
-    fields.push({title: "```/makeACall", value: "(Coming Soon) Make a Call and Advance Cadence", short: false});
-    fields.push({title: "```/sendAnEmail", value: "(Coming Soon) Send an Email and Advance Cadence", short: false});
+   	fields.push({title: "`/whoami`", value: "User Info", short: false});
+    fields.push({title: "`/cadences`", value: "List of Sales Cadences", short: false});
+    fields.push({title: "`/targets`", value: "List of recent Targets (Lead, Contact, Person Account)", short: false});
+    fields.push({title: "`/wq`", value: "Work Queue", short: false});
+    fields.push({title: "`/addToCadence`", value: "Assign Target to Sales Cadence", short: false});
+    fields.push({title: "`/removeFromCadence`", value: "Remove Target from Sales Cadence", short: false});
+    fields.push({title: "`/makeACall`", value: "(Coming Soon) Make a Call and Advance Cadence", short: false});
+    fields.push({title: "`/sendAnEmail`", value: "(Coming Soon) Send an Email and Advance Cadence", short: false});
     
 	attachments.push({
 	    color: "#2019FB",
